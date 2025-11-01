0f8e5c22e4092dfb401447d1b923b78c26987792
PlayerDoni <donovanteixeiradelima@gmail.com>
2025-10-31 22:01:22 -0300

    Primeiro commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SupabaseModule } from './supabase/supabase.module';
+import { UserModule } from './cases/users/user.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT), 
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true, 
+      synchronize: false, 
+    }), SupabaseModule, UserModule
+  ],
+})
+export class AppModule {}
